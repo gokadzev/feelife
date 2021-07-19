82f679da0a8c911ed0ec46ef538ac9228728abdd
Valeri Gokadze <gokadze.v1@gmail.com>
2021-07-19 20:22:21 +0400

    improved perfomance

--- src/app/shared_services/content-global-refresher.service.ts
+++ src/app/shared_services/content-global-refresher.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Album } from '../shared_models/album.model';
+import { Playlist } from '../shared_models/playlist.model';
 import { PLsinger } from '../shared_models/singer.model';
 import { PLsong } from '../shared_models/song.model';
 import { DataexchangerService } from './dataexchanger.service';
@@ -14,6 +15,10 @@ export class ContentGlobalRefresherService {
   constructor(private httpserv:HttpserviceService, public router:Router, private dataexchanger:DataexchangerService) { }
 
   favoriteSongs:any [] = [];
+  songs:PLsong[];
+  singers:PLsinger[];
+  playlists:Playlist[];
+  albums:Album[];
   
   
 
@@ -26,17 +31,23 @@ export class ContentGlobalRefresherService {
   getData(type:string){
 
     if(type == 'songs'){
+      if(this.songs == undefined){
       this.httpserv.getSubscribableData(this.apiUrl).subscribe(songs=>{
         var tempdata = JSON.stringify(songs)
         var convertedData = JSON.parse(tempdata);
+        this.songs = convertedData;
         this.dataexchanger.songs.emit(convertedData);
       }); 
-  
-    } else if (type == 'singers'){
+    } else {
+      this.dataexchanger.songs.emit(this.songs);
+    }
 
+    } else if (type == 'singers'){
+      if(this.songs == undefined && this.singers == undefined){
       this.httpserv.getSubscribableData(this.apiUrl).subscribe(songs=>{
         var tempdata = JSON.stringify(songs)
         var convertedData = JSON.parse(tempdata);
+        this.songs = convertedData;
         var singers:PLsinger [] = [];
         var singer = 'something'
 
@@ -46,16 +57,36 @@ export class ContentGlobalRefresherService {
             singer = convertedData[i].coverphoto
           }
         }
-
+        this.singers = singers;
         this.dataexchanger.singers.emit(singers);
-      }); 
+      })} else if(this.songs != undefined && this.singers == undefined){
+        var singers:PLsinger [] = [];
+        var singer = 'something'
+
+        for(var i = 0; i < this.songs.length; i++){
+          if(singer != this.songs[i].coverphoto){
+            singers.push(new PLsinger(i,this.songs[i].singer,this.songs[i].coverphoto))
+            singer = this.songs[i].coverphoto
+          }
+        }
+        this.singers = singers;
+        this.dataexchanger.singers.emit(singers);
+      } else if(this.singers != undefined){
+        this.dataexchanger.singers.emit(this.singers);
+      }
     } else if (type == 'playlists'){
+      if(this.playlists == undefined){
       this.httpserv.getSubscribableData(this.playlistsApiUrl).subscribe(playlists=>{
         var tempdata = JSON.stringify(playlists)
         var convertedData = JSON.parse(tempdata);
+        this.playlists = convertedData;
         this.dataexchanger.playlists.emit(convertedData);
-      }); 
+      });
+    } else {
+      this.dataexchanger.playlists.emit(this.playlists);
+    } 
     } else if (type == "shuffledSongs"){
+      if(this.songs == undefined){
       var shuffledArray:PLsong[] = [];
       var songsList:PLsong[];
       this.httpserv.getSubscribableData(this.apiUrl).subscribe(songs=>{
@@ -73,10 +104,28 @@ export class ContentGlobalRefresherService {
         }
       }); 
       this.dataexchanger.shuffledArray.emit(shuffledArray);
+    } else if(this.songs != undefined){
+      var shuffledArray:PLsong[] = [];
+
+      for(var i = 0; i <15; i++){
+        var randNum:number = Math.round(Math.random() * this.songs.length)
+        if(this.songs[randNum].id != undefined){
+          var tempArray = shuffledArray.filter(s => s.id == this.songs[randNum].id);
+        }
+        if(tempArray.length == 0){
+          shuffledArray.push(this.songs[randNum])
+          tempArray = null;
+        }
+      }
+
+      this.dataexchanger.shuffledArray.emit(shuffledArray);
+    }
     } else if(type == "songs&singers"){
+      if(this.songs == undefined && this.singers == undefined){
       this.httpserv.getSubscribableData(this.apiUrl).subscribe(songs=>{
         var tempdata = JSON.stringify(songs)
         var convertedData = JSON.parse(tempdata);
+        this.songs = convertedData;
         this.dataexchanger.songs.emit(convertedData);
 
         var singers:PLsinger [] = [];
@@ -89,13 +138,27 @@ export class ContentGlobalRefresherService {
             singer = convertedData[i].coverphoto
           }
         }
+        this.singers = singers;
         this.dataexchanger.singers.emit(singers);
       }); 
-    } else if(type="albums"){
-
+    } else if (this.songs == undefined && this.singers != undefined){
       this.httpserv.getSubscribableData(this.apiUrl).subscribe(songs=>{
         var tempdata = JSON.stringify(songs)
         var convertedData = JSON.parse(tempdata);
+        this.songs = convertedData;
+        this.dataexchanger.songs.emit(convertedData);
+      }); 
+      this.dataexchanger.singers.emit(singers);
+    } else if(this.songs != undefined && this.singers != undefined){
+      this.dataexchanger.songs.emit(this.songs);
+      this.dataexchanger.singers.emit(this.singers);
+    }
+    } else if(type="albums"){
+      if(this.songs == undefined && this.albums == undefined){
+      this.httpserv.getSubscribableData(this.apiUrl).subscribe(songs=>{
+        var tempdata = JSON.stringify(songs)
+        var convertedData = JSON.parse(tempdata);
+        this.songs = convertedData;
         var convertedData1:Album [] = [];
 
         for(var i =0; i < convertedData.length; i++){
@@ -104,9 +167,23 @@ export class ContentGlobalRefresherService {
             convertedData1.push(new Album(convertedData[i].album,convertedData[i].albumCover))
           }
         }
-        
+        this.albums = convertedData1;
         this.dataexchanger.albums.emit(convertedData1);
       }); 
+    } else if (this.songs != undefined && this.albums == undefined){
+      var convertedData1:Album [] = [];
+
+      for(var i =0; i < this.songs.length; i++){
+        let filteredArray = convertedData1.filter(value => value.album === this.songs[i].album)
+        if(filteredArray.length == 0){
+          convertedData1.push(new Album(this.songs[i].album,this.songs[i].albumCover))
+        }
+      }
+      this.albums = convertedData1;
+      this.dataexchanger.albums.emit(convertedData1);
+    } else if (this.songs != undefined && this.albums != undefined){
+      this.dataexchanger.albums.emit(this.albums);
+    }
     }
 
   }
